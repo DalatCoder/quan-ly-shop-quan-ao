2175cb7d905bfd7ddd97c666a975206052553b94
Trong Hieu <hieuntctk42@gmail.com>
2020-10-30 21:40:05 +0700

    Bổ sung script tự động tạo comment sql đẹp 🐱‍👤🐱‍👤🐱‍👤

---
+++ makeString.py
@@ -0,0 +1,26 @@
+from sys import argv
+from math import floor
+
+if (len(argv) == 1):
+    print("python makeString.py 'Exampe' [box]")
+    exit(0)
+
+max_length = 80
+title = argv[1].trim()
+title = ' ' + title + ' '
+title_length = len(title)
+
+is_box = False
+if (len(argv) > 2):
+    if (argv[2] == 'box'):
+        is_box = True
+
+num_pad_start = floor((max_length - title_length) / 2)
+num_pad_end = max_length - title_length - num_pad_start
+
+result_string = '-' * num_pad_start + title + '-' * num_pad_end
+
+if is_box:
+    result_string = f"{'-'*80}\n{result_string}\n{'-'*80}"
+
+print(result_string)
