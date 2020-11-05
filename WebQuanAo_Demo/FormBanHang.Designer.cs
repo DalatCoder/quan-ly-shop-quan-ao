b5e6c69025384c1e75044ccc6e1a203ce98b9619
Trong Hieu <hieuntctk42@gmail.com>
2020-11-05 15:19:59 +0700

    Cập nhật form bán hàng

--- WebQuanAo_Demo/FormBanHang.Designer.cs
+++ WebQuanAo_Demo/FormBanHang.Designer.cs
@@ -28,15 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Quần thun",
-            "10.000",
-            "10"}, -1);
-			System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Áo thun",
-            "5.000",
-            "5"}, -1);
-			this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
 			this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
 			this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
 			this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
@@ -46,31 +37,25 @@
 			this.btnThoat = new DevExpress.XtraEditors.SimpleButton();
 			this.btnTimKiemQA = new DevExpress.XtraEditors.SimpleButton();
 			this.txtSoDT = new DevExpress.XtraEditors.TextEdit();
-			this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
-			this.lvChiTietSanPham = new System.Windows.Forms.ListView();
-			this.colTenSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.colDonGia = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.colSoLuong = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
 			this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+			this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+			this.labelControl10 = new DevExpress.XtraEditors.LabelControl();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tsslTotal = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.dgvNguoiDung = new System.Windows.Forms.DataGridView();
 			((System.ComponentModel.ISupportInitialize)(this.txtMaGiaoDich.Properties)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtTenKhachHang.Properties)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtSoDT.Properties)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+			this.statusStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvNguoiDung)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// labelControl1
-			// 
-			this.labelControl1.Location = new System.Drawing.Point(405, 35);
-			this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-			this.labelControl1.Name = "labelControl1";
-			this.labelControl1.Size = new System.Drawing.Size(167, 17);
-			this.labelControl1.TabIndex = 0;
-			this.labelControl1.Text = "Thông tin hóa đơn bán hàng";
 			// 
 			// labelControl2
 			// 
-			this.labelControl2.Location = new System.Drawing.Point(76, 132);
+			this.labelControl2.Location = new System.Drawing.Point(52, 80);
 			this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.labelControl2.Name = "labelControl2";
 			this.labelControl2.Size = new System.Drawing.Size(77, 17);
@@ -79,7 +64,7 @@
 			// 
 			// labelControl4
 			// 
-			this.labelControl4.Location = new System.Drawing.Point(40, 192);
+			this.labelControl4.Location = new System.Drawing.Point(20, 117);
 			this.labelControl4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.labelControl4.Name = "labelControl4";
 			this.labelControl4.Size = new System.Drawing.Size(109, 17);
@@ -88,7 +73,7 @@
 			// 
 			// labelControl3
 			// 
-			this.labelControl3.Location = new System.Drawing.Point(72, 245);
+			this.labelControl3.Location = new System.Drawing.Point(52, 154);
 			this.labelControl3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.labelControl3.Name = "labelControl3";
 			this.labelControl3.Size = new System.Drawing.Size(77, 17);
@@ -97,26 +82,26 @@
 			// 
 			// txtMaGiaoDich
 			// 
-			this.txtMaGiaoDich.Location = new System.Drawing.Point(210, 132);
+			this.txtMaGiaoDich.Location = new System.Drawing.Point(135, 77);
 			this.txtMaGiaoDich.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.txtMaGiaoDich.Name = "txtMaGiaoDich";
-			this.txtMaGiaoDich.Size = new System.Drawing.Size(737, 24);
+			this.txtMaGiaoDich.Size = new System.Drawing.Size(653, 24);
 			this.txtMaGiaoDich.TabIndex = 5;
 			// 
 			// txtTenKhachHang
 			// 
-			this.txtTenKhachHang.Location = new System.Drawing.Point(210, 188);
+			this.txtTenKhachHang.Location = new System.Drawing.Point(135, 114);
 			this.txtTenKhachHang.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.txtTenKhachHang.Name = "txtTenKhachHang";
-			this.txtTenKhachHang.Size = new System.Drawing.Size(737, 24);
+			this.txtTenKhachHang.Size = new System.Drawing.Size(653, 24);
 			this.txtTenKhachHang.TabIndex = 6;
 			// 
 			// btnThemHoaDon
 			// 
-			this.btnThemHoaDon.Location = new System.Drawing.Point(80, 624);
+			this.btnThemHoaDon.Location = new System.Drawing.Point(305, 578);
 			this.btnThemHoaDon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.btnThemHoaDon.Name = "btnThemHoaDon";
-			this.btnThemHoaDon.Size = new System.Drawing.Size(134, 30);
+			this.btnThemHoaDon.Size = new System.Drawing.Size(192, 30);
 			this.btnThemHoaDon.TabIndex = 8;
 			this.btnThemHoaDon.Text = "Xác nhận mua hàng";
 			// 
@@ -131,71 +116,28 @@
 			// 
 			// btnTimKiemQA
 			// 
-			this.btnTimKiemQA.Location = new System.Drawing.Point(405, 624);
+			this.btnTimKiemQA.Location = new System.Drawing.Point(305, 197);
 			this.btnTimKiemQA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.btnTimKiemQA.Name = "btnTimKiemQA";
 			this.btnTimKiemQA.Size = new System.Drawing.Size(192, 30);
 			this.btnTimKiemQA.TabIndex = 10;
-			this.btnTimKiemQA.Text = "Tìm kiếm quần áo";
+			this.btnTimKiemQA.Text = "Chọn sản phẩm";
 			// 
 			// txtSoDT
 			// 
-			this.txtSoDT.Location = new System.Drawing.Point(210, 241);
+			this.txtSoDT.Location = new System.Drawing.Point(135, 150);
 			this.txtSoDT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.txtSoDT.Name = "txtSoDT";
 			this.txtSoDT.Properties.AutoHeight = false;
 			this.txtSoDT.Properties.Mask.UseMaskAsDisplayFormat = true;
 			this.txtSoDT.Properties.MaskSettings.Set("MaskManagerType", typeof(DevExpress.Data.Mask.SimpleMaskManager));
 			this.txtSoDT.Properties.MaskSettings.Set("mask", "000.000.0000");
-			this.txtSoDT.Size = new System.Drawing.Size(737, 26);
+			this.txtSoDT.Size = new System.Drawing.Size(653, 26);
 			this.txtSoDT.TabIndex = 11;
-			// 
-			// labelControl5
-			// 
-			this.labelControl5.Location = new System.Drawing.Point(429, 290);
-			this.labelControl5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-			this.labelControl5.Name = "labelControl5";
-			this.labelControl5.Size = new System.Drawing.Size(152, 17);
-			this.labelControl5.TabIndex = 12;
-			this.labelControl5.Text = "Chi tiết sản phẩm đã chọn";
-			// 
-			// lvChiTietSanPham
-			// 
-			this.lvChiTietSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.colTenSP,
-            this.colDonGia,
-            this.colSoLuong});
-			this.lvChiTietSanPham.GridLines = true;
-			this.lvChiTietSanPham.HideSelection = false;
-			this.lvChiTietSanPham.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2});
-			this.lvChiTietSanPham.Location = new System.Drawing.Point(40, 336);
-			this.lvChiTietSanPham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-			this.lvChiTietSanPham.Name = "lvChiTietSanPham";
-			this.lvChiTietSanPham.Size = new System.Drawing.Size(907, 200);
-			this.lvChiTietSanPham.TabIndex = 13;
-			this.lvChiTietSanPham.UseCompatibleStateImageBehavior = false;
-			this.lvChiTietSanPham.View = System.Windows.Forms.View.Details;
-			// 
-			// colTenSP
-			// 
-			this.colTenSP.Text = "Tên Sản Phẩm";
-			this.colTenSP.Width = 276;
-			// 
-			// colDonGia
-			// 
-			this.colDonGia.Text = "Đơn Giá";
-			this.colDonGia.Width = 161;
-			// 
-			// colSoLuong
-			// 
-			this.colSoLuong.Text = "Số Lượng";
-			this.colSoLuong.Width = 103;
 			// 
 			// labelControl6
 			// 
-			this.labelControl6.Location = new System.Drawing.Point(148, 571);
+			this.labelControl6.Location = new System.Drawing.Point(148, 522);
 			this.labelControl6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.labelControl6.Name = "labelControl6";
 			this.labelControl6.Size = new System.Drawing.Size(121, 17);
@@ -205,7 +147,7 @@
 			// textEdit2
 			// 
 			this.textEdit2.EditValue = "125.000";
-			this.textEdit2.Location = new System.Drawing.Point(357, 568);
+			this.textEdit2.Location = new System.Drawing.Point(357, 519);
 			this.textEdit2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.textEdit2.Name = "textEdit2";
 			this.textEdit2.Properties.AppearanceReadOnly.Options.UseTextOptions = true;
@@ -214,14 +156,89 @@
 			this.textEdit2.Size = new System.Drawing.Size(281, 24);
 			this.textEdit2.TabIndex = 15;
 			// 
+			// labelControl1
+			// 
+			this.labelControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelControl1.Appearance.Options.UseFont = true;
+			this.labelControl1.Appearance.Options.UseTextOptions = true;
+			this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+			this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+			this.labelControl1.Location = new System.Drawing.Point(0, 20);
+			this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 20);
+			this.labelControl1.Name = "labelControl1";
+			this.labelControl1.Size = new System.Drawing.Size(808, 33);
+			this.labelControl1.TabIndex = 16;
+			this.labelControl1.Text = "Hóa đơn bán hàng";
+			// 
+			// labelControl10
+			// 
+			this.labelControl10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelControl10.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelControl10.Appearance.Options.UseFont = true;
+			this.labelControl10.Appearance.Options.UseTextOptions = true;
+			this.labelControl10.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+			this.labelControl10.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+			this.labelControl10.Location = new System.Drawing.Point(0, 248);
+			this.labelControl10.Margin = new System.Windows.Forms.Padding(3, 20, 3, 20);
+			this.labelControl10.Name = "labelControl10";
+			this.labelControl10.Size = new System.Drawing.Size(808, 33);
+			this.labelControl10.TabIndex = 42;
+			this.labelControl10.Text = "Danh sách sản phẩm đã chọn";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.BackColor = System.Drawing.Color.Transparent;
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslTotal,
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 633);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(808, 55);
+			this.statusStrip1.TabIndex = 43;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// tsslTotal
+			// 
+			this.tsslTotal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.tsslTotal.Margin = new System.Windows.Forms.Padding(20);
+			this.tsslTotal.Name = "tsslTotal";
+			this.tsslTotal.Size = new System.Drawing.Size(163, 15);
+			this.tsslTotal.Text = "Tổng só sản phẩm đã chọn: 2";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(212, 50);
+			this.toolStripStatusLabel1.Text = "Tổng số tiền phải thanh toán: 125.000đ";
+			// 
+			// dgvNguoiDung
+			// 
+			this.dgvNguoiDung.AllowUserToAddRows = false;
+			this.dgvNguoiDung.AllowUserToDeleteRows = false;
+			this.dgvNguoiDung.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvNguoiDung.BackgroundColor = System.Drawing.Color.Azure;
+			this.dgvNguoiDung.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			this.dgvNguoiDung.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvNguoiDung.Location = new System.Drawing.Point(21, 289);
+			this.dgvNguoiDung.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.dgvNguoiDung.MultiSelect = false;
+			this.dgvNguoiDung.Name = "dgvNguoiDung";
+			this.dgvNguoiDung.RowHeadersWidth = 51;
+			this.dgvNguoiDung.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.dgvNguoiDung.Size = new System.Drawing.Size(766, 208);
+			this.dgvNguoiDung.TabIndex = 44;
+			// 
 			// FormBanHang
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.dgvNguoiDung);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.labelControl10);
+			this.Controls.Add(this.labelControl1);
 			this.Controls.Add(this.textEdit2);
 			this.Controls.Add(this.labelControl6);
-			this.Controls.Add(this.lvChiTietSanPham);
-			this.Controls.Add(this.labelControl5);
 			this.Controls.Add(this.txtSoDT);
 			this.Controls.Add(this.btnTimKiemQA);
 			this.Controls.Add(this.btnThoat);
@@ -231,7 +248,6 @@
 			this.Controls.Add(this.labelControl3);
 			this.Controls.Add(this.labelControl4);
 			this.Controls.Add(this.labelControl2);
-			this.Controls.Add(this.labelControl1);
 			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.Name = "FormBanHang";
 			this.Size = new System.Drawing.Size(808, 688);
@@ -239,14 +255,15 @@
 			((System.ComponentModel.ISupportInitialize)(this.txtTenKhachHang.Properties)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtSoDT.Properties)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvNguoiDung)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.LabelControl labelControl4;
         private DevExpress.XtraEditors.LabelControl labelControl3;
@@ -256,12 +273,13 @@
         private DevExpress.XtraEditors.SimpleButton btnThoat;
         private DevExpress.XtraEditors.SimpleButton btnTimKiemQA;
         private DevExpress.XtraEditors.TextEdit txtSoDT;
-        private DevExpress.XtraEditors.LabelControl labelControl5;
-        private System.Windows.Forms.ListView lvChiTietSanPham;
-        private System.Windows.Forms.ColumnHeader colTenSP;
-        private System.Windows.Forms.ColumnHeader colDonGia;
-        private System.Windows.Forms.ColumnHeader colSoLuong;
         private DevExpress.XtraEditors.LabelControl labelControl6;
         private DevExpress.XtraEditors.TextEdit textEdit2;
-    }
+		private DevExpress.XtraEditors.LabelControl labelControl1;
+		private DevExpress.XtraEditors.LabelControl labelControl10;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel tsslTotal;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.DataGridView dgvNguoiDung;
+	}
 }
