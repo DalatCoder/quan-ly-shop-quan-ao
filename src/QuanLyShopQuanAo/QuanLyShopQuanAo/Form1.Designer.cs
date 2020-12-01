03975c7b4717fe2f1fafd0e878b97cdf27b61e21
LeAiMy <hantctk42@gmail.com>
2020-12-01 10:28:47 +0700

    Bo sung

--- src/QuanLyShopQuanAo/QuanLyShopQuanAo/Form1.Designer.cs
+++ src/QuanLyShopQuanAo/QuanLyShopQuanAo/Form1.Designer.cs
@@ -45,7 +45,7 @@ namespace QuanLyShopQuanAo
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(946, 643);
+			this.ClientSize = new System.Drawing.Size(1064, 681);
 			this.Controls.Add(this.label1);
 			this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
