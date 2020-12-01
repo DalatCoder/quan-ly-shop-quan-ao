08616135a1f957277fa912aed09346c8bf3a54a5
Trong Hieu <hieuntctk42@gmail.com>
2020-12-01 09:46:17 +0700

    Reset 👻👻👻

--- WebQuanAo_Demo/Program.cs
+++ WebQuanAo_Demo/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using DevExpress.UserSkins;
-using DevExpress.Skins;
-using System.Globalization;
 
 namespace WebQuanAo_Demo
 {
@@ -18,7 +15,7 @@ namespace WebQuanAo_Demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Login());
+            Application.Run(new frmLogin());
         }
     }
 }
--- WebQuanAo_Demo/frmLogin.cs
+++ WebQuanAo_Demo/frmLogin.cs
@@ -10,18 +10,11 @@ using System.Windows.Forms;
 
 namespace WebQuanAo_Demo
 {
-	public partial class Form_Product_Choice : DevExpress.XtraEditors.XtraForm
+	public partial class frmLogin : Form
 	{
-		public Form_Product_Choice()
+		public frmLogin()
 		{
 			InitializeComponent();
 		}
-
-        private void btnThoat_Click(object sender, EventArgs e)
-        {
-			Application.Exit();
-        }
-
-      
-    }
+	}
 }
