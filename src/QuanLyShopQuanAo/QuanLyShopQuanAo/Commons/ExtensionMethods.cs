c1aef4624e27f08e7fac841cd921010299497c33
Trong Hieu <hieuntctk42@gmail.com>
2020-12-05 20:25:37 +0700

    hide columns on datagridview

---
+++ src/QuanLyShopQuanAo/QuanLyShopQuanAo/Commons/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShopQuanAo.Commons
+{
+	public static class ExtensionMethods
+	{
+		public static void HideColumns(this DataGridView dgv, params string[] columns)
+		{
+			foreach (string column in columns)
+			{
+				dgv.Columns[column].Visible = false;
+			}
+		}
+	}
+}
